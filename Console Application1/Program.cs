b91aa5d20dc9388b494f2101c9b02d9602f288ce
Lijun <1330018801@qq.com>
2016-12-04 10:33:58 +0800

    Add a line to print HelloWorld to the console.

--- Console Application1/Program.cs
+++ Console Application1/Program.cs
@@ -9,6 +9,8 @@ namespace Console_Application1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, world!");
+            Console.Read();
         }
     }
 }
